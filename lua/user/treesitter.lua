39f580bbe32f58e718eb53b949df7352c811c405
Philipp Eder <philipp.eder@posteo.net>
2022-08-01 10:19:53 +0200

    Change: use a list of languages instead of all

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -3,10 +3,10 @@ if not status_ok then
   return
 end
 
-configs.setup {
-  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+configs.setup({
+  ensure_installed = { "c", "rust", "go", "json", "javascript", "python", "html", "toml", "regex", "swift" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  ignore_install = { "" }, -- List of parsers to ignore installing
+  ignore_install = {}, -- List of parsers to ignore installing
   autopairs = {
     enable = true,
   },
@@ -20,4 +20,4 @@ configs.setup {
     enable = true,
     enable_autocmd = false,
   },
-}
+})
